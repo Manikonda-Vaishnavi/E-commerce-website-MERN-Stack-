817032ed281a93a9bd57d2d4eaa602a50db2030c
M.vaishnavi <vaishnavimanikonda2525@gmail.com>
2024-08-01 23:33:55 +0530

    first commit

---
+++ frontedn-zip/src/customer/Components/Carousel/HomeCaroselData.js
@@ -0,0 +1,26 @@
+export const homeCarouselData=[
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/_/1_12.jpg" ,
+        path:"/Users/vaishu/Pictures/pic"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/5/_/5_13.jpg" ,
+        path:"/Users/vaishu/Pictures/pic"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/2/_/2_17.jpg",
+        path:"/Users/vaishu/Pictures/pic"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/4/_/4_18.jpg" ,
+        path:"/Users/vaishu/Pictures/pic"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/_/1_12.jpg" ,
+        path:"/Users/vaishu/Pictures/pic"
+    }
+    
+
+]
+
+
